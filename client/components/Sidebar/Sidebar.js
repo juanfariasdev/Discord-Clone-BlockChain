b6323c04be95b8dab25690a81713e3ebc2a2cda5
Juan Pablo Farias <juanpablo.p.a.f@gmail.com>
2022-04-16 19:27:24 -0300

    Created Sidebar

---
+++ client/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Sidebar.module.css";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const Sidebar = () => {
+  return <div className={styles.wrapper}>Sidebar</div>;
+};
+
+export default Sidebar;
