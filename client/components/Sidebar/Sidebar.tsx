fbb59179c7b36bc4f24c0423f80e05611db9dc52
Juan Pablo Farias <juanpablo.p.a.f@gmail.com>
2022-05-31 20:42:31 -0300

    basic configure dummyChannel

--- client/components/Sidebar/Sidebar.tsx
+++ client/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,47 @@ import styles from "./Sidebar.module.scss";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
+import avatar1 from '../../assets/avatar-1.webp'
+import avatar2 from '../../assets/avatar-2.png'
+import avatar3 from '../../assets/avatar-3.webp'
+import avatar4 from '../../assets/avatar-4.webp'
+import { StaticImageData } from "next/image";
+
+
+interface IChannel  {
+  id: number,
+  name: string,
+  avatar: StaticImageData,
+}
+
+const dummyChannel:IChannel[] = [
+  {
+    id: 1,
+    name: "general",
+    avatar: avatar1
+  },
+  {
+    id: 2,
+    name: "random",
+    avatar: avatar2
+  },
+  {
+    id: 3,
+    name: "chill",
+    avatar: avatar3
+  },  
+  {
+    id: 4,
+    name: "buildspace",
+    avatar: avatar4
+  }
+]
+
+
 const Sidebar = () => {
+  const router = useRouter();
+const [channels, setChannels] = useState(dummyChannel);
+
   return <div className={styles.wrapper}>Sidebar</div>;
 };
 
